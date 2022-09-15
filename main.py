4eb26fb5ee0da1597cbefc3659a817003249e832
eliasjcm <eliasc20022002@gmail.com>
2022-09-15 16:54:08 +0000

    edge_detection function

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
+import os
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
-# img = cv2.imread("edificio_con_arboles.jpg")
-img = cv2.imread("el-paisaje-urbano-europeo-combinado-con-la-naturaleza-berna-es-una-inusual-capital-europea-su-perfecta-mezcla-rADo-aare-da-otra-164795497.jpg")
-# img = cv2.imread("popular.jpg")
-img_color = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+def edge_detection(img_path):
+    print(img_path)
+    # img = cv2.imread("edificio_con_arboles.jpg")
+    # img_path = input("Ingrese la ruta de la imagen: ")
+    img = cv2.imread(img_path)
+    # img = cv2.imread("popular.jpg")
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imwrite("gray.jpg", img_color)
+    # cv2.imwrite("gray.jpg", img_color)
 
-height, width = img_color.shape[:2]
-new_image = np.zeros((height, width), np.uint8)
+    height, width = img_gray.shape[:2]
+    print(f"height: {height}, width: {width}")
+    new_image = np.zeros((height, width), np.uint8)
 
-
-for y in range(height):
     for x in range(width):
-        if x - 1 >= 0:
-            derivative = abs(int(img_color[y, x]) - int(img_color[y, x-1]))
-            new_image[y, x] = derivative
-        if y - 1 >= 0:
-            derivative = abs(int(img_color[y, x]) - int(img_color[y-1, x]))
-            new_image[y, x] = (new_image[y, x] + derivative) % 256
+        for y in range(height):
+            if x - 1 >= 0:
+                derivative = abs(int(img_gray[y, x]) - int(img_gray[y, x-1]))
+                new_image[y, x] = derivative
+            if y - 1 >= 0:
+                derivative = abs(int(img_gray[y, x]) - int(img_gray[y-1, x]))
+                new_image[y, x] = min((new_image[y, x] + derivative), 255)
+
+    cv2.imwrite(f"../bordes/BORDES-{img_path}", new_image)
+    # print(new_image[0, :])
 
 
-cv2.imwrite("new_image_2.jpg", new_image)
-# plt.imshow(img_color)
+os.chdir("./discord-imagenes")
+for img in os.listdir():
+    edge_detection(img)
