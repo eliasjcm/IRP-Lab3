129f3710e904d80bca62db42fc9f532b7b577af2
Abiel Porras Garro <66501176+juuzou13@users.noreply.github.com>
2022-09-27 20:29:50 -0600

    g

--- MapeoBilineal.py
+++ MapeoBilineal.py
@@ -2,11 +2,6 @@ import cv2
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-# from skimage import io
-# from google.colab.patches import cv2_imshow
-
-img = cv2.imread('example.png')
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def tiene_mapeo(a: complex, b: complex, c: complex, d: complex):
   print(f"{b}*{c}-{a}*{d}={b*c-a*d}")
@@ -93,17 +88,20 @@ def newImageInterAux(img, a, b, c, d, inter8):
   mapeo = crear_mapeo_inverso(a, b, c, d)
   new_img = np.zeros((new_width, new_height), np.uint8)
 
+  print(f"new_width: {new_width}, new_height: {new_height}")
+  print(f"img.shape[0]: {img.shape[0]}, img.shape[1]: {img.shape[1]}")
+
   for x in range(new_width):
     for y in range(new_height):
       z = mapeo(complex(x, y))
       new_x = int(z.real)
       new_y = int(z.imag)
-      if not (new_x == 0 or new_x == (img.shape[0]-1) or new_y == 0 or new_y == (img.shape[0]-1)):
-        if 0 <= new_x < img.shape[0] and 0 <= new_y < img.shape[1]:
-          if inter8:
-            new_img[x, y] = interpolacion8(img, new_x, new_y)
-          else:
-            new_img[x, y] = interpolacion4(img, new_x, new_y)
+      if 0 < new_x < img.shape[0]-1 and 0 < new_y < img.shape[1]-1:
+        # print(f"({x}, {y}) -> ({new_x}, {new_y})")
+        if inter8:
+          new_img[x, y] = interpolacion8(img, new_x, new_y)
+        else:
+          new_img[x, y] = interpolacion4(img, new_x, new_y)
   return new_img
 
 def newImageInterpolation4(img, a, b, c, d):
@@ -122,15 +120,18 @@ def newImageAB(img, a, b):
   new_img = newImage(img, a, b, 0, 1)
   return new_img
 
-# new_img = newImage(img_gray,  2+2.1j, 0, 0.004, 4+6j) 1.3
-# new_img = newImageAB(img_gray,  3, 0) 1.4.1
-# new_img = newImageAB(img_gray,  3+1j, 0) 1.4.2
-# new_img = newImageAB(img_gray,  1, 600+450j) 1.4.3
-# new_img = newImageAB(img_gray,  3+1j, 400+300j) 1.4.4
+img = cv2.imread('example.png')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-new_img = newImage(img_gray, 3+2.1j, 0, 0.004, 4+6j)
+new_img = newImageInterpolation8(img_gray, 2.1+2.1j, 0, 0.003, 1+1j)
+# new_img = newImage(img_gray, 2.1+2.1j, 0, 0.003, 1+1j)
 
-plt.imshow(new_img)
+# mapeo = crear_mapeo_inverso(2.1+2.1j, 0, 0.003, 1+1j)
+# print(f"({410}, {274}) -> ({int(mapeo(complex(410, 274)).real)}, {int(mapeo(complex(410, 274)).imag)})")
+
+blur = addGaussianBlur(new_img, 5)
+
+plt.imshow(blur, cmap='gray')
 plt.show()
 
-cv2.imwrite("1.3.png", new_img)
+cv2.imwrite("imagen8.png", blur)
