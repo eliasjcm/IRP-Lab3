e91ac75b8fa4785c59b2e8f72194e73ba30c8804
Elias <eliasc20022002@gmail.com>
2022-10-07 00:39:46 -0600

    fix

--- Lab05.py
+++ Lab05.py
@@ -22,18 +22,17 @@ def interpolation4(img, x, y):
     return count
 
 
-def chessPixels(img, color):
+def create_chess_pattern(img, color: list) -> np.ndarray:
+    """
+    Función utilizada para crear el patrón de tablero de ajedrez coloreando la imagen con el color recibido.
+    Los pixeles se pintan de forma que no hayan dos pixeles contiguos ni vertical ni horizontalmente pintados del mismo color recobido
+    """
     height, width = img.shape[:2]
-    for y in range(height):
-        if y % 2 == 0:
-            for x in range(width):
-                if x % 2 == 0:
-                    img[y][x] = color
-        else:
-            for x in range(width):
-                if x % 2 != 0:
-                    img[y][x] = color
-    return img
+    for i in range(height):
+        for j in range(width):
+            # evita que dos pixeles contiguos sean pintados del mismo color
+            if (i + j) % 2:
+                img[i, j] = color
 
 
 def getPixelsInterpolation(img):
@@ -42,15 +41,17 @@ def getPixelsInterpolation(img):
     picked_color = img[0][0]
     for x in range(height):
         for y in range(width):
-            if (img[x][y] == picked_color).all():
-                print(img[x][y])
-                if not (x == 0 or x == (height-1) or y == 0 or y == (width-1)):
-                    neighbours = np.array(
-                        [img[x, y-1], img[x, y+1], img[x-1, y], img[x+1, y]])
-                    new_img[x][y] = np.average(neighbours)
-            else:
-                new_img[x][y] = img[x][y]
-                    # img[x, y] = interpolation4(img, x, y)
+            if (x + y) % 2 and not (x == 0 or x == (height-1) or y == 0 or y == (width-1)):
+                neighbours = np.sum(
+                    [img[x, y-1], img[x, y+1], img[x-1, y], img[x+1, y]], axis=0) / 4
+                # print neighbours values
+
+                # print(img[x, y-1], img[x, y+1], img[x-1, y],
+                #       img[x+1, y], interpolation4(img,x,y), neighbours)
+                new_img[x][y] = neighbours
+                # print(np.average(neighbours))
+    np.clip(new_img, 0, 255, out=new_img)
+    # img[x, y] = interpolation4(img, x, y)
     return new_img
 
 
@@ -76,61 +77,26 @@ def highlightEdges(img, img_name, ksize):
 
 
 def main():
-    # img = cv2.imread('example.png')
-    # img = convertGrayscale(img)
-    # img = chessPixels(img, (0,0,0))
-    # cv2.imwrite('chess.png', img)
-    # # plt.imshow(img, cmap='gray')
-    # # plt.show()
-    # img = getPixelsInterpolation(img)
-    # # plt.imshow(img, cmap='gray')
-    # # plt.show()
-    # highlightEdges(img, 'exampleLaplace.png', 3)
-    # cv2.imwrite('example2.png', img)
     ddepth = cv2.CV_16S
     kernel_size = 3
-    window_name = "Laplace Demo"
-    # [variables]
-    # [load]
-    imageName = 'example.png'
+    imageName = '1.jpg'
     src = cv2.imread(cv2.samples.findFile(imageName),
                      cv2.IMREAD_COLOR)  # Load an image
 
     # Check if image is loaded fine
     if src is None:
         print('Error opening image')
-        print('Program Arguments: [image_name -- default lena.jpg]')
         return -1
-    # [load]
-    # [reduce_noise]
-    # Remove noise by blurring with a Gaussian filter
-    src = chessPixels(src, (255, 255, 255))
 
-    # src = cv2.GaussianBlur(src, (3, 3), 0)
-    # src = cv2.resize(src, dsize=(src.shape[1], src.shape[0]), interpolation=cv2.INTER_CUBIC)
+    create_chess_pattern(src, (0, 0, 0))
+
     src = getPixelsInterpolation(src)
-    # [reduce_noise]
-    # [convert_to_gray]
-    # Convert the image to grayscale
-    # src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    # [convert_to_gray]
-    # Create Window
-    # cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
-    # [laplacian]
-    # Apply Laplace function
     dst = cv2.Laplacian(src, ddepth, ksize=kernel_size)
-    # [laplacian]
-    # [convert]
-    # converting back to uint8
     abs_dst = cv2.convertScaleAbs(dst)
-    print(abs_dst)
-    # [convert]
-    # [display]
-    final = cv2.add(src, abs_dst)
+    c = .2
+    final = src + c * abs_dst
     cv2.imwrite('exampleLaplace_antes.png', src)
     cv2.imwrite('exampleLaplace_despues.png', final)
-    # cv2.imshow(window_name, final)
-    # cv2.waitKey(0)
 
 
 main()
